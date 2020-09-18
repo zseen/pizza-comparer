167f2979f0856c36fbf8083a393c19628d1270cf
zseen <43151120+zseen@users.noreply.github.com>
2020-09-18 01:03:06 +0100

    Start implementing the handling of the unit of measurement for diameter

--- ComparePizzas/src/InputHandler.java
+++ ComparePizzas/src/InputHandler.java
@@ -1,17 +1,33 @@
 import java.util.Scanner;
 
 public class InputHandler {
+	public enum UnitOfMeasurement {
+		CM("cm"),
+		INCH("inch");
+
+		private final String measurement;
+
+		UnitOfMeasurement(String measurement) {
+			this.measurement = measurement;
+		}
+
+		public String geUnitOfMeasurementName() {
+			return this.measurement;
+		}
+
+	}
+
 	public Scanner sc = new Scanner(System.in);
 
 	public Pizza createPizzaFromUserInput(String pizzaSequenceNum) {
 		Pizza pizza = new Pizza();
 		double pizzaDiameter = getPizzaAttributeFromUser(pizzaSequenceNum, "diameter");
+		
+		String diameterUnitOfMeasurement = getDiameterUnitOfMeasurementFromUser();
+		//System.out.printf("MEASUREMENT: %s", diameterUnitOfMeasurement);
 
 		double pizzaPrice = getPizzaAttributeFromUser(pizzaSequenceNum, "price");
-		while (pizzaPrice < 0) {
-			System.out.printf("The price of a pizza cannot be less than 0! ");
-			pizzaPrice = getPizzaAttributeFromUser(pizzaSequenceNum, "price");
-		}
+		
 
 		pizza.setDiameter(pizzaDiameter);
 		pizza.setPrice(pizzaPrice);
@@ -38,6 +54,37 @@ public class InputHandler {
 
 		}
 
+	}
+	
+	private String getDiameterUnitOfMeasurementFromUser() {
+		//System.out.printf("in getDiameterUnit() ");
+	
+		
+		String unitOfMeasurement = null;
+		//System.out.printf("so it got here");
+		
+		while (unitOfMeasurement == null) {
+			//System.out.printf("uom: %s", unitOfMeasurement);
+			System.out.printf("What unit of measurement is your pizza? Inch `inch` or `cm`?: ");
+			unitOfMeasurement = sc.next();
+			
+		}
+		
+		//System.out.printf("In `getDiameterUnitOfMeasurementFromUser`, unitOFMeasurement: %s \n", unitOfMeasurement);
+		//System.out.printf("enum value cm: %s \n", UnitOfMeasurement.CM.measurement);
+		//System.out.print(UnitOfMeasurement.CM.measurement.equals(unitOfMeasurement) );
+		
+		
+		while (!unitOfMeasurement.equals(UnitOfMeasurement.CM.measurement) && !unitOfMeasurement.equals(UnitOfMeasurement.INCH.measurement)) {
+			//System.out.printf("In while loop, unitOFMeasurement: %s \n", unitOfMeasurement);
+			//System.out.printf("The diameter should be `cm` or `inch`!: ");
+			sc.next();
+
+			
+		}
+		return unitOfMeasurement;
+		
+		
 	}
 
 }
